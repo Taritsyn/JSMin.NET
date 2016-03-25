c1188619472673e4694933a960eb9a7f274e147f
Andrey Taritsyn <taritsyn@gmail.com>
2016-03-25 18:49:50 +0300

    Was made refactoring

--- NuGet/build-package.cmd
+++ NuGet/build-package.cmd
@@ -1,2 +1,13 @@
-\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\src\DouglasCrockford.JsMin\DouglasCrockford.JsMin.csproj /p:Configuration=Release
-..\.nuget\nuget.exe pack JSMin.NET.nuspec
+set net40_msbuild=\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+set nuget_package_manager=..\.nuget\nuget.exe
+
+set project_name=DouglasCrockford.JsMin
+set project_source_dir=..\src\%project_name%
+
+rmdir lib /Q/S
+
+%net40_msbuild% %project_source_dir%\%project_name%.Net40.csproj /p:Configuration=Release
+xcopy %project_source_dir%\bin\Release\%project_name%.dll lib\net40-client\
+copy ..\LICENSE license.txt /Y
+
+%nuget_package_manager% pack %project_name%.nuspec
